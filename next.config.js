5f1cd6d66f71663399c92e25ddb7f0f6933e560b
mateus1102senai <mateus.marcelino@aluno.senai.br>
2025-11-13 13:49:14 -0300

    Configuração inicial do Next.js com TypeScript e Tailwind CSS

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost'],
+  },
+  env: {
+    CUSTOM_KEY: 'cantina-crud',
+  },
+}
+
+module.exports = nextConfig
